f0f222b25a614aa127683fc94548d0ceddc56881
Macr0s <matte0809@gmail.com>
2014-05-29 12:00:59 +0200

    La classe TagVincente è stata commentata

--- src/it/uniroma3/diadia/ambienti/parser/tag/TagVincente.java
+++ src/it/uniroma3/diadia/ambienti/parser/tag/TagVincente.java
@@ -2,6 +2,14 @@ package it.uniroma3.diadia.ambienti.parser.tag;
 
 import it.uniroma3.diadia.ambienti.parser.Tag;
 
+/**
+ * Questa classe gestisce i Tag Vincenti
+ * 
+ * @author Matteo Filippi, Andrea Salvoni
+ * @see Tag
+ * @version 0.4
+ *
+ */
 public class TagVincente extends Tag {
 
 	@Override
