9deffdda7c2df21e64207829aacdd9a09a809472
Macr0s <matte0809@gmail.com>
2014-05-29 11:37:58 +0200

    La classe TagStanze è stata commentata

--- src/it/uniroma3/diadia/ambienti/parser/tag/TagStanze.java
+++ src/it/uniroma3/diadia/ambienti/parser/tag/TagStanze.java
@@ -3,6 +3,14 @@ package it.uniroma3.diadia.ambienti.parser.tag;
 import it.uniroma3.diadia.ambienti.Stanza;
 import it.uniroma3.diadia.ambienti.parser.Tag;
 
+/**
+ * Questa classe gestisce i tag Stanze
+ * 
+ * @author Matteo Filippi, Andrea Salvoni
+ * @see Tag
+ * @version 0.4
+ *
+ */
 public class TagStanze extends Tag {
 
 	@Override
