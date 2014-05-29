a207134a1b862abcbb12ada5a651699471151522
Macr0s <matte0809@gmail.com>
2014-05-29 11:24:38 +0200

    La classe TagMaghi è stata commentata

--- src/it/uniroma3/diadia/ambienti/parser/tag/TagMaghi.java
+++ src/it/uniroma3/diadia/ambienti/parser/tag/TagMaghi.java
@@ -4,6 +4,15 @@ import it.uniroma3.diadia.ambienti.parser.FormatoFileNonValidoException;
 import it.uniroma3.diadia.ambienti.parser.Tag;
 import it.uniroma3.diadia.personaggi.Mago;
 
+/**
+ * Questa classe gestisce i tag Maghi
+ *
+ * @author Matteo Filippi, Andrea Salvoni
+ * @see Tag
+ * @see FormatoFileNonValidoException
+ * @version 0.4
+ *
+ */
 public class TagMaghi extends Tag {
 
 	@Override
