c8f34b8bd6c58bf7bb0a95d57fc61805d8a2f926
Macr0s <matte0809@gmail.com>
2014-06-06 12:11:09 +0200

    Caricamento base di codice test Strega

---
+++ test/it/uniroma3/diadia/personaggi/StregaTest.java
@@ -0,0 +1,24 @@
+package it.uniroma3.diadia.personaggi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StregaTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void testAgisci() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRiceviRegalo() {
+		fail("Not yet implemented");
+	}
+}
